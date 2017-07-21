bc90eb0d6191f9b0075b7bd18ef5eb3384c40e4c
Nafeu Nasir <nafeu.nasir@gmail.com>
2017-07-20 20:07:37 -0400

    Add slide visibility and navigation

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,66 @@
+var body,
+    main,
+    htmlArray,
+    slideIndex = 0,
+    maxSlideIndex,
+    converter = new showdown.Converter();
+
 $(document).ready(function(){
 
-  var main = $("#main"),
-      converter = new showdown.Converter();
+  // Selectors
+  body = $("body");
+  main = $("#main");
 
+  // Markdown parsing logic
   $.get('data/lecture1.md').done(function(data){
     var html = converter.makeHtml(data);
-    var htmlArray = html.split("\n<p>+++</p>\n");
+    htmlArray = html.split("\n<p>+++</p>\n");
+    maxSlideIndex = htmlArray.length - 1;
+
     console.log(htmlArray);
 
-    htmlArray.forEach(function(slide){
-
-      main.append(createSlide(slide));
-
+    htmlArray.forEach(function(slide, index){
+      main.append(createSlide(slide, index));
     });
 
+    displaySlide();
+  });
+
+  // Keyboard inputs
+  $(this).keydown(function(e){
+    e = e || window.event;
+    e.preventDefault();
+    if (e.keyCode == '37') {
+       prevSlide();
+    }
+    else if (e.keyCode == '39') {
+       nextSlide();
+    }
   });
 
 });
 
-function createSlide(slide) {
-  return $("<div>", {class: "slide"}).html(slide);
+function createSlide(slide, index) {
+  return $("<div>", {class: "slide", id: "slide-"+index}).html(slide);
+}
+
+function displaySlide() {
+  $(".slide").hide();
+  $("#slide-"+slideIndex).show();
+}
+
+function nextSlide() {
+  if (slideIndex < maxSlideIndex) {
+    slideIndex++;
+    displaySlide();
+  }
+  return slideIndex;
+}
+
+function prevSlide() {
+  if (slideIndex > 0) {
+    slideIndex--;
+    displaySlide();
+  }
+  return slideIndex;
 }
